824e129de8f184396b76cde87d2c07a2cfa0ddb9
Andrew Patrick <andrew.prsilva@gmail.com>
2020-03-02 17:18:00 -0300

    Post page & post method complete

--- src/main/java/com/blogspot/main/controller/BlogController.java
+++ src/main/java/com/blogspot/main/controller/BlogController.java
@@ -1,12 +1,18 @@
 package com.blogspot.main.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.blogspot.main.model.Post;
 import com.blogspot.main.services.PostService;
@@ -36,6 +42,16 @@ public class BlogController {
 	@GetMapping(value = "/newpost")
 	public String getPostsForm() {
 		return "postForm";
+	}
+
+	@PostMapping(value = "/newpost")
+	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			return "redirect:/newpost";
+		}
+		post.setDate(LocalDate.now());
+		postService.save(post);
+		return "redirect:/posts";
 	}
 
 }
